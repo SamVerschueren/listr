5157edfcaaf32af74dd6e56d2b195b429b70e9b1
Sam Verschueren <sam.verschueren@gmail.com>
2018-11-17 20:59:34 +0100

    Bump dependencies

--- test/fixtures/utils.js
+++ test/fixtures/utils.js
@@ -5,7 +5,7 @@ exports.testOutput = (t, expected) => {
 	t.plan(t._test.planCount || expected.length);
 	let i = 0;
 
-	const unhook = hookStd(actual => {
+	return hookStd.stdout((actual, unhook) => {
 		t.is(actual, `${expected[i++]}\n`);
 
 		if (i === expected.length) {
