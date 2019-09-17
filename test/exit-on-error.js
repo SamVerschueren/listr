c0317c71704b277b65f44ee03134d8e0c452ca35
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2019-09-17 19:10:18 +0000

    unit-test for suspending update-rendering added

--- test/exit-on-error.js
+++ test/exit-on-error.js
@@ -1,7 +1,7 @@
 import {serial as test} from 'ava';
-import Listr from '..';
 import SimpleRenderer from './fixtures/simple-renderer';
 import {testOutput} from './fixtures/utils';
+import Listr from '..';
 
 const tasks = [
 	{
---
+++ test/suspend.js
@@ -0,0 +1,68 @@
+import {serial as test} from 'ava';
+import sinon from 'sinon';
+
+const mockery = require('mockery');
+
+const logUpdateApi = {
+	main(text) {
+		console.log(text);
+	}
+};
+logUpdateApi.main.clear = function () {
+};
+
+logUpdateApi.main.done = function () {
+};
+
+const mock = sinon.mock(logUpdateApi);
+
+function getTasks() {
+	// Require is necessary at this position, otherwise mockery is not working correctly
+	const Listr = require('..');
+	const tasks = new Listr([
+		{
+			title: 'not suspending task',
+			task: () => {
+				return new Promise(resolve => {
+					setTimeout(() => {
+						resolve('predefined output');
+					}, 200);
+				});
+			}
+		},
+		{
+			title: 'suspending task',
+			task: () => {
+				return new Promise(resolve => {
+					setTimeout(() => {
+						resolve('predefined output');
+					}, 4000);
+				});
+			},
+			options: {suspendUpdateRenderer: true}
+		}
+	]);
+
+	return tasks;
+}
+
+test.before(() => {
+	/* One time for the first task,
+	   one time when both tasks are finished
+	*/
+	mock.expects('main').twice();
+	mockery.registerAllowable('..');
+	mockery.registerMock('log-update', logUpdateApi.main);
+	mockery.enable({useCleanCache: true, warnOnUnregistered: false});
+});
+
+test('should suspend second task', async t => {
+	await getTasks().run();
+
+	t.true(mock.verify());
+});
+
+test.after(() => {
+	mockery.disable();
+	mockery.deregisterAll();
+});
